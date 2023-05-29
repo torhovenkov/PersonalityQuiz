73503b003e480bcb5bf3a786adcebcbb5a71b715
torhovenkov <vladislav.lnx@gmail.com>
2023-05-29 06:02:30 +0300

    Added array for keeping all questions and answers to QuestionViewController Added new file Questions.swift to hold definitions of Question, Answer types

---
+++ PersonalityQuiz/Question.swift
@@ -0,0 +1,40 @@
+//
+//  Question.swift
+//  PersonalityQuiz
+//
+//  Created by Vladyslav Torhovenkov on 29.05.2023.
+//
+
+import Foundation
+
+struct Question {
+    var text: String
+    var type: ResponseType
+    var answers: [Answer]
+}
+
+enum ResponseType {
+    case single, multiple, ranged
+}
+
+struct Answer {
+    var text: String
+    var type: AnimalType
+}
+
+enum AnimalType: Character {
+    case dog = "🐶", cat = "🐱", turtle = "🐢", rabbit = "🐇"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surround yourself with the people you love and enjoy activities with your friends."
+        case .cat:
+            return "Mischievous, yet mild-tempered, you enjoy doing things on your own terms."
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+        case .rabbit:
+            return "You love everything that’s soft. You are healthy and full of energy."
+        }
+    }
+}
